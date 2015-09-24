2e4e05cbc39f2e2b4becc49cc6465c0e7db10b53
Aishwary Tandon <ashwarytandon@gmail.com>
2015-09-23 23:29:23 -0400

    A view Injection Library For android

---
+++ app/src/main/java/com/tandon/aishwary/butterknifedemo/SimpleApp.java
@@ -0,0 +1,13 @@
+package com.tandon.aishwary.butterknifedemo;
+
+/**
+ * Created by Aishwary on 9/23/2015.
+ */
+import android.app.Application;
+
+public class SimpleApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
